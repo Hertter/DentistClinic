e7537c966634736811e1cb15a7b84a3c07a3897a
GAhlin <844123045@qq.com>
2020-07-09 21:11:18 +0800

    完成接口调用

---
+++ web/js/caseList.js
@@ -0,0 +1,90 @@
+// 遍历查询病例
+$(function load() {
+    $.ajax({
+        contentType:'application/x-www-form-urlencoded;charset=utf-8',
+        type: "POST",
+        dataType: "json",
+        url: "http://dentist-clinic.com:8088/php/Case.php",
+        data: {
+            operate: "query",
+        },
+        success:function(data){
+            console.log(data);
+            var listdata=data.result;
+            if(listdata.length>0){
+                var listInfo="";
+                $.each(listdata,function(){
+					listInfo+="<div class='media col-sm-12 mt-sm-0 mt-3'>"+
+                                    "<div class='media-left'>"+
+                                        "<a href='#'>"+
+                                            "<img class='media-object' src='images/default-avatar.png'>"+
+                                        "</a>"+
+                                    "</div>"+
+                                    "<div class='media-body'>"+
+                                        "<h4 class='media-heading'><a href='caseDetails.html?"+this.id+"'>"+this.name+"</a></h4>"+
+                                        "<b>编号：<span>"+this.id+"</span></b>"+
+                                        "<br>病例详情:<br>&emsp;&emsp;"+
+                                        "<span>"+this.note+"</span>"+
+                                    "</div>"+
+                                    "<div class='login-icon work-icon'>"+
+                                        "<a href='#' data-id='"+this.id+"' onclick='delCase("+this.id+")'> 删除</a>"+
+                                    "</div>"+
+                                "</div><br>";
+                });
+                $("#caseList")[0].innerHTML=listInfo;	
+            }
+        },
+        error:function(data){
+            console.log(data);
+        }
+    });
+});
+
+// 添加病例
+function addCase() {
+    var addCaseForm = $('#addCaseForm').serialize();
+    $.ajax({
+        contentType:'application/x-www-form-urlencoded;charset=utf-8',
+        type: "POST",
+        dataType: "json",
+        url: "http://dentist-clinic.com:8088/php/Case.php",
+        data: $.param({operate: "add"})+'&'+addCaseForm,
+        success: function (data) {
+            console.log(data.result);
+            if("病例添加成功"==(data.result)){
+                alert(data.result);
+                location.reload();
+            }else{
+                alert(data.result);
+            }
+        },
+        error:function(data) {
+            console.log(data.result);
+            alert(data);
+        }
+    });
+}
+
+// 删除病例
+function delCase(id) {
+    if(confirm("此操作不可逆，确认删除吗")){
+        $.ajax({
+            contentType:'application/x-www-form-urlencoded;charset=utf-8',
+            type: "POST",
+            dataType: "json",
+            url: "http://dentist-clinic.com:8088/php/Case.php",
+            data: {
+                operate:"delete",
+                id:id
+            },
+            success: function (data) {
+                console.log(data);
+                location.reload()
+            },
+            error:function(data) {
+                console.log(data);
+            }
+        })
+    }
+};
+
--- web/js/common.js
+++ web/js/common.js
@@ -1,3 +1,4 @@
+// 修改资料切换
 $(document).ready(function(){
     $("#modifyData").click(function(){ 
         $("#personDetailData").toggleClass("switch",true); 
@@ -11,6 +12,8 @@ $(document).ready(function(){
         $("#personModifyData").toggleClass("switch",true); 
     }); 
 })
+
+// 覆盖头像
 var upload = function (c, d) {
     "use strict";
     var $c = document.querySelector(c),
@@ -22,6 +25,121 @@ var upload = function (c, d) {
         $d.setAttribute("src", e.target.result);
     };
 };
+
+//跳转修改页面
+function caseModify() {
+    var id = sessionStorage.getItem('id');
+    window.location.href="http://dentist-clinic.com:8080/web/caseModify.html?"+id;
+}
+
+// 修改病例
+function modifyCase() {
+
+    var strHref = window.location.href;
+    var s = strHref.indexOf("?");
+    var id = strHref.substring(s+1);
+
+    var modifyCaseForm = $('#modifyCaseForm').serialize();
+    var note = $("#note").val().toString();
+    var plan = $("#plan").val().toString();
+
+    $.ajax({
+        contentType:'application/x-www-form-urlencoded;charset=utf-8',
+        type: "POST",
+        dataType: "json",
+        url: "http://dentist-clinic.com:8088/php/Case.php",
+        data: $.param({
+            operate: "update",
+            note: note,
+            treatment_plan:plan,
+            id:id
+        })+'&'+modifyCaseForm,
+        success: function (data) {
+            console.log(data);
+            alert(data.result);
+            if("更新成功"==(data.result)){
+                window.location.href="http://dentist-clinic.com:8080/web/caseDetails.html?"+id;
+            }
+        },
+        error:function(data) {
+            console.log(data);
+            alert(data.result);
+        }
+    });
+}
+
+//修改页面回显
+$(function modifyDetails() {
+    var name = sessionStorage.getItem('name');
+    var sex = sessionStorage.getItem('sex');
+    var status = sessionStorage.getItem('status');
+    var born = sessionStorage.getItem('born');
+
+    $("#name").val(name);
+    $("#sex").val(sex);
+    $("#status").val(status);
+    $("#born_year").val(born);
+});
+
+//跳转回病例详情页面
+function returnCaseDetails() {
+
+    var strHref = window.location.href;
+    var s = strHref.indexOf("?");
+    var id = strHref.substring(s+1);
+    window.location.href="http://dentist-clinic.com:8080/web/caseDetails.html?"+id;
+};
+
+// 根据id查询病例
+$(function caseFind() {
+    var strHref = window.location.href;
+    var s = strHref.indexOf("?");
+    var id = strHref.substring(s+1);
+    $.ajax({
+        contentType:'application/x-www-form-urlencoded;charset=utf-8',
+        type: "POST",
+        dataType: "json",
+        url: "http://dentist-clinic.com:8088/php/Case.php",
+        data: {
+            operate: "find",
+            id: id
+        },
+        success:function(data){
+            console.log(data);
+            var id = data.result[0].id;
+            var name = data.result[0].name;
+            var sex = data.result[0].sex;
+            var status = data.result[0].status;
+            var treatment_plan = data.result[0].treatment_plan;
+            var note = data.result[0].note;
+            var born_year = data.result[0].born_year;
+
+            sessionStorage.setItem('id', id);
+            sessionStorage.setItem('name', name);
+            sessionStorage.setItem('status', status);
+            sessionStorage.setItem('treatment_plan', treatment_plan);
+            sessionStorage.setItem('sex', sex);
+            sessionStorage.setItem('born', born_year);
+            sessionStorage.setItem('note', note);
+
+
+            $("#id").html(id);
+            $("#name").html(name);
+            $("#sex").html(sex);
+            $("#status").html(status);
+            $("#born_year").html(born_year);
+            $("#plan").html(treatment_plan);
+            $("#note").html(note);
+
+
+            
+        },
+        error:function(data){
+            console.log(data);
+        }
+    });
+});
+
 
 
 
